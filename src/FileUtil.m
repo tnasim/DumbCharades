9a398b9cf063bf4178717467b92fe5a69534ccf6
Tariq M Nasim <tnasim@asu.edu>
2018-03-08 15:01:32 -0700

    Added code in test.m to read sensor data file

--- src/FileUtil.m
+++ src/FileUtil.m
@@ -1,10 +1,12 @@
 classdef FileUtil
     
     properties (Constant)
+        dataBaseDirectory = '../Data';
+        
         PARAMETERS_FILE_NAME = 'Parameters.csv';
         ACTION_ID_MAPPING_FILE_NAME = 'ActionIDMapping.csv';
         ACTION_ID_DATA_MAPPING_FILE_NAME = 'ActionIDDataMapping.csv';
-        dataBaseDirectory = '../Data';
+        SENSOR_DATA_DIRECTORY_NAME = 'ParameterWiseData';
     end
     
     methods (Static)
@@ -81,6 +83,35 @@ classdef FileUtil
             while tempParameter ~= -1
                 Parameters = [Parameters;tempParameter];
                 tempParameter = fgetl(fileID);
+            end
+            fclose(fileID);
+            a = Parameters;
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Read the Sensor Data from file
+        %   Each line contains the sensor data for the given sensor.
+        % returns rows of sensor data
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function a = readSensorData(sensorName)
+            fileName = strcat(sensorName, '.csv');
+            filePath = strcat(FileUtil.dataBaseDirectory,'/', FileUtil.SENSOR_DATA_DIRECTORY_NAME, '/', fileName);
+            a = csvread(filePath);
+            return
+            [fileID,message] = fopen( [FileUtil.dataBaseDirectory,'/', FileUtil.SENSOR_DATA_DIRECTORY_NAME, '/', fileName], 'rt' );
+            if fileID == -1
+                error('''%s'' read Error: %s', fileName, message);
+            end
+            
+            i=1;
+            tempLine = fgetl(fileID);
+            Parameters = {};
+            while tempLine ~= -1
+                dataTempLine = regexp(tempLine, ',', 'split');
+                tempElementsNo = numel(dataTempLine);
+                Parameters(i,:) = dataTempLine(1:tempElementsNo);
+                tempLine = fgetl(fileID);
+                i = i + 1;
             end
             fclose(fileID);
             a = Parameters;
--- src/test.m
+++ src/test.m
@@ -1,3 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SAMPLE CODE TO READ PARAMETER AND MAPPING FILES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 listParam = FileUtil.getListOfParameters();
 listActionIDMappings = FileUtil.readActionIDMappings();
-listActionIDDataMappings = FileUtil.readActionIDDataMappings()
+listActionIDDataMappings = FileUtil.readActionIDDataMappings();
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SAMPLE CODE TO READ SENSOR DATA FILES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sensorDataMatrix = FileUtil.readSensorData('ALX');
