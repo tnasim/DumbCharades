69c9f79ee4d0b09be44babeb87bc5a7d57959fa9
Tariq M Nasim <tnasim@asu.edu>
2018-03-24 00:46:12 -0700

    updated code and report as of submission

--- src/PCA.m
+++ src/PCA.m
@@ -22,13 +22,16 @@ classdef PCA
                 tempFoldername = dirListing(i).name;
                 %disp(tempFoldername);
                 if ~contains(tempFoldername,'.','IgnoreCase',true)
+                    %{
                     if(strcmp( gesture, 'go out') )
                         % TODO --- regex for 'go out'
+                        
                         fileName = [PCA.IntermediateDataDir,'/',tempFoldername,'/','go*','.csv'];
                         %disp(fileName);
                     else
+                    %}
                         fileName = [PCA.IntermediateDataDir,'/',tempFoldername,'/',gesture,'.csv'];
-                    end
+                    %end
                     if exist(fileName, 'file') == 2
                         temp = csvread([PCA.IntermediateDataDir,'/',tempFoldername,'/',gesture,'.csv'],0, 0);
                         [x,z] = size(temp);
@@ -66,16 +69,30 @@ classdef PCA
                     FM = std(M, 0, 2);
                 case 'FFT'
                     FM = fft(M, [], 2);
+                case 'DWT2'
+                    [FM, ~, ~, ~] = dwt2(M, 'db1');
                 case 'DWT'
-                    [FM, ~, ~, ~] = dwt2(M, 'haar');
+                    FM = PCA.getDWT(M);
                 case 'MAD2'
                     FM = mad(M,0,2); % MEAN
+                case 'VAR'
+                    FM = var(M,0,2); % Variance
                 otherwise
                     disp('wrong feautre extraction method');
             end
         end
         
-        function ABOUT_FEATURE_MAT = getFeatureMatrix_ABOUT()
+        function A = getDWT(M)
+            [x, y] = size(M);
+            A = zeros(x, ceil(y/2));
+            for i = 1:x
+                [A(i, :),~] = dwt(M(i,:), 'haar');
+            end
+        end
+        
+        % Creating Feature Matrix for ABOUT.
+        % appending the features MAD(OPR), RMS(OPR) and MAD(EMG0R)
+        function FEATURE_MAT = getFeatureMatrix_ABOUT()
             gest = 'about';
             
             M = PCA.readOneSensorData(gest, Sensor.OPR);
@@ -88,8 +105,253 @@ classdef PCA
 
             % Concat all the feature columns to get
             %  the whole feature matrix
-            ABOUT_FEATURE_MAT = [OPR_MAD, OPR_RMS, EMG0R_MAD];
+            FEATURE_MAT = [OPR_MAD, OPR_RMS, EMG0R_MAD];
         end
+        
+        % Creating Feature Matrix for FATHER.
+        function FEATURE_MAT = getFeatureMatrix_FATHER()
+            gest = 'father';
+            
+            M1 = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F1 = PCA.getFeatureMatrix(M1,'DWT');
+            
+            M2 = PCA.readOneSensorData(gest, Sensor.OPR);
+            F2 = PCA.getFeatureMatrix(M2,'RMS');
+
+            M3 = PCA.readOneSensorData(gest, Sensor.EMG0R);
+            F3 = PCA.getFeatureMatrix(M3,'MAD');
+
+            % Concat all the feature columns to get
+            %  the whole feature matrix
+            FEATURE_MAT = [F1, F2, F3];
+        end
+        
+        % Creating Feature Matrix for AND.
+        function FEATURE_MAT = getFeatureMatrix_AND()
+            gest = 'and';
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARX);
+
+            F_1 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0R);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG2R);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        
+        % Creating Feature Matrix for CAN.
+        function FEATURE_MAT = getFeatureMatrix_CAN()
+            gest = 'can';
+            
+            % TODO -- xcorr
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0R);
+
+            F_1 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0L);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ALY);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        % Creating Feature Matrix for FIND.
+        function FEATURE_MAT = getFeatureMatrix_FIND()
+            gest = 'find';
+            
+            M = PCA.readOneSensorData(gest, Sensor.GRY);
+
+            F_1 = PCA.getFeatureMatrix(M,'FFT');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARX);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0R);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG2R);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        % Creating Feature Matrix for COP.
+        function FEATURE_MAT = getFeatureMatrix_COP()
+            gest = 'cop';
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARZ);
+
+            F_1 = PCA.getFeatureMatrix(M,'STD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ORR);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.OPR);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3];
+        end
+        
+        % Creating Feature Matrix for DEAF.
+        function FEATURE_MAT = getFeatureMatrix_DEAF()
+            gest = 'deaf';
+            
+            % TODO -- xcorr
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARX);
+
+            F_1 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0L);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.GRX);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ORR);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        % Creating Feature Matrix for DECIDE.
+        function FEATURE_MAT = getFeatureMatrix_DECIDE()
+            gest = 'decide';
+            
+            M = PCA.readOneSensorData(gest, Sensor.ALY);
+
+            F_1 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG3L);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ORR);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        
+        % Creating Feature Matrix for GO OUT.
+        function FEATURE_MAT = getFeatureMatrix_GO_OUT()
+            gest = 'go out';
+            
+            M = PCA.readOneSensorData(gest, Sensor.EMG0R);
+
+            F_1 = PCA.getFeatureMatrix(M,'RMS');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F_2 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.GRX);
+
+            F_3 = PCA.getFeatureMatrix(M,'MAD');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ORR);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        % Creating Feature Matrix for HEARING.
+        function FEATURE_MAT = getFeatureMatrix_HEARING()
+            gest = 'hearing';
+            
+            M = PCA.readOneSensorData(gest, Sensor.OPR);
+
+            F_1 = PCA.getFeatureMatrix(M,'VAR');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARZ);
+
+            F_2 = PCA.getFeatureMatrix(M,'RMS');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARY);
+
+            F_3 = PCA.getFeatureMatrix(M,'RMS');
+            
+            M = PCA.readOneSensorData(gest, Sensor.ARX);
+
+            F_4 = PCA.getFeatureMatrix(M,'MAD');
+            
+            FEATURE_MAT = [F_1, F_2, F_3, F_4];
+        end
+        
+        
+        % Get the row-wise Cross-correlation of two matrices
+        %{
+        function xcor = getCrossCorrelation(M1, M2)
+            [r1, c1] = size(M1);
+            [r2, c2] = size(M2);
+            minr = min(r1, r2)
+            
+            % assuming c1 == c2 (column numbers are same for the two matrices)
+            M = zeros(minr, c1);
+            for i = 1:minr
+                M(i, :) = xcorr( M1(i, :), M2(i, :));
+            end
+            
+            xcor = M;
+            
+        end
+        %}
+        
+        %{
+        function [e] = plotev(coeffM)
+           %  [e] = plotev(n)
+           %
+           %  This function creates a random matrix of square
+           %  dimension (n).  It computes the eigenvalues (e) of 
+           %  the matrix and plots them in the complex plane.
+           %
+           
+           e = eig(coeffM);    % Get the eigenvalues of A
+
+           close all    % Closes all currently open figures.
+           figure(1)
+             plot(real(e),imag(e),'r*') %   Plot real and imaginary parts
+             xlabel('Real')
+             ylabel('Imaginary')
+             t1 = ['Eigenvalues of a random matrix of dimension ' num2str(n)];
+             title(t1)
+        end
+        %}
         
     end
 end
--- src/test_PCA.m
+++ src/test_PCA.m
@@ -7,21 +7,178 @@
 % ABOUT
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+
 ABT_FEATURE_MAT = PCA.getFeatureMatrix_ABOUT();
-[coeff,score,latent] = pca(ABT_FEATURE_MAT);
+[coeff,score,latent,tsquared,explained] = pca(ABT_FEATURE_MAT);
 biplot(coeff(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3'});
+title('PCA plot for ABOUT');
+% legend('v1-MAD(OPR)','v2-RMS(OPR)','v3-MAD(EMG0R)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('SCREE plot for ABOUT');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DEAF
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+FEATURE_MAT = PCA.getFeatureMatrix_DEAF();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+%PCA.plotev(coeff1);
+title('PCA plot for DEAF');
+% legend('v1-MAD(ARX)','v2-VAR(EMG0L)','v3-MAD(GRX)','v4-MAD(ORR)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('SCREE plot for DEAF');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AND
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FEATURE_MAT = PCA.getFeatureMatrix_AND();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for AND');
+% legend('v1-VAR(ARX)','v2-VAR(ARY)','v3-MAD(EMG0R)','v4-MAD(EMG2R)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for AND');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% HEARING
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+FEATURE_MAT = PCA.getFeatureMatrix_HEARING();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for HEARING');
+% legend('v1-VAR(OPR)','v2-RMS(ARZ)','v3-STD(GRX)','v4-MAD(ORR)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for HEARING');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CAN
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FEATURE_MAT = PCA.getFeatureMatrix_CAN();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for CAN');
+% legend('v1-VAR(EMG0R)','v2-VAR(EMG0L)','v3-MAD(ALY)','v4-MAD(ARY)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for CAN');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FEATURE_MAT = PCA.getFeatureMatrix_COP();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3'});
+title('PCA plot for COP');
+% legend('v1-STD(ARZ)','v2-VAR(OPR)','v3-MAD(OPR)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for COP');
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DECIDE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FEATURE_MAT = PCA.getFeatureMatrix_DECIDE();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for DECIDE');
+% legend('v1-VAR(ALY)','v2-VAR(ARY)','v3-MAD(EMG3L)','v4-MAD(ORR)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for DECIDE');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GO_OUT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FEATURE_MAT = PCA.getFeatureMatrix_GO_OUT();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(:,1:2),'scores',score(:,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for GO OUT');
+% legend('v1-RMS(EMG0R)','v2-VAR(ARY)','v3-MAD(GRX)','v4-MAD(ORR)');
+
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for GO OUT');
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FIND
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+FEATURE_MAT = PCA.getFeatureMatrix_FIND();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(1:4,1:2),'scores',score(1:4,1:2),'varlabels',{'v_1','v_2','v_3', 'v_4'});
+title('PCA plot for FIND');
+% legend('v1-FFT(GRY)','v2-VAR(ARX)','v3-MAD(EMG0R)','v4-MAD(EMG2R)');
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for FIND');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % FATHER
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-M = PCA.readOneSensorData('father', Sensor.ARY);
-FATHER_ARY = PCA.getFeatureMatrix(M,'DWT');
 
-[coeff,score,latent] = pca(FATHER_ARY);
-% Multiply 1st row of the feature matrix using the first eigenvactor on the left.
-% mat = coeff(:, 1) * FATHER_ARY(1, :);
+FEATURE_MAT = PCA.getFeatureMatrix_FATHER();
+[coeff1,score,latent,tsquared,explained] = pca(FEATURE_MAT);
+biplot(coeff1(1:3,1:2),'scores',score(1:3,1:2),'varlabels',{'v_1','v_2','v_3'});
+%biplot(coeff1(28:30,1:2),'scores',score(28:30,1:2),'varlabels',{'v_1','v_2','v_3'});
+title('PCA plot for FATHER');
+% legend('v1-DWT(ARY)','v2-RMS(OPR)','v3-MAD(EMG0R)');
 
-% Make a biplot of the first 4 components
-%biplot(coeff(1:3,1:3),'scores',score(1:3,1:3),'varlabels',{'v_1','v_2','v_3'});
+figure();
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title('Scree plot  for FATHER');
+
 
