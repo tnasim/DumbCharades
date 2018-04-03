a28266483dff8e841718c77744ec512132b20325
Tariq M Nasim (Student) <tnasim@sundevils.asu.edu>
2018-04-03 16:58:42 -0700

    little comment added

--- src/test_A3.m
+++ src/test_A3.m
@@ -1,3 +1,6 @@
+% Make sure you have put the PCATask2 folder (with data) in the 'Data'
+% folder.
+
 [train, test] = A3.getClassData('about', 80);
 %[train, test] = A3.getClassData('and', 80);
 %[train, test] = A3.getClassData('can', 80);
